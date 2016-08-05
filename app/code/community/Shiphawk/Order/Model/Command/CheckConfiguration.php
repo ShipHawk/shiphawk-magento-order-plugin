1fb9a557a66a5d14ba24ed43ff1fb6b12da59348
Kanishka Reddy <kanishka.reddy@shiphawk.com>
2016-08-05 11:54:32 -0700

    1. updating drop down options for Order Sync config
    2. added error message when no store name or phone number

--- app/code/community/Shiphawk/Order/Model/Command/CheckConfiguration.php
+++ app/code/community/Shiphawk/Order/Model/Command/CheckConfiguration.php
@@ -10,6 +10,11 @@ class Shiphawk_Order_Model_Command_CheckConfiguration
         $client->setHeaders('X-Api-Key', $key);
 
 
+        if(!(Mage::getStoreConfig('general/store_information/name')) ||  !(Mage::getStoreConfig('general/store_information/phone'))){
+            Mage::getSingleton('adminhtml/session')
+                ->addError(Mage::helper('shiphawk_order')->__('Missing information required for printing labels: Please add a store name and phone number under System > Configuration > General > Store Information'));
+        }
+
         $response = $client->request(Zend_Http_Client::GET);
 
         if ($response->isSuccessful()) {
@@ -21,5 +26,6 @@ class Shiphawk_Order_Model_Command_CheckConfiguration
                 ->addError(Mage::helper('shiphawk_order')->__('Unable to authenticate API key.'));
             Mage::getConfig()->saveConfig('shiphawk/order/status', 0);
         }
+
     }
 }
