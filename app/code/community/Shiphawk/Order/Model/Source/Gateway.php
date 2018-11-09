50f8d55611d2cc4393310bf002a55a97fe6d04fd
Alexander Lukin <aleaxnder.lukin@shiphawk.com>
2018-11-09 12:05:01 +0200

    [#161789685] add ability to use RC env.

--- app/code/community/Shiphawk/Order/Model/Source/Gateway.php
+++ app/code/community/Shiphawk/Order/Model/Source/Gateway.php
@@ -9,7 +9,8 @@ class Shiphawk_Order_Model_Source_Gateway
             array('value'=>'https://sandbox.shiphawk.com/api/v4/', 'label' => Mage::helper('adminhtml')->__('Sandbox')),
             array('value'=>'https://stage.shiphawk.com/api/v4/', 'label' => Mage::helper('adminhtml')->__('STAGE')),
             array('value'=>'https://qa.shiphawk.com/api/v4/', 'label' => Mage::helper('adminhtml')->__('QA')),
-            array('value'=>'http://127.0.0.1:3000/api/v4/', 'label' => Mage::helper('adminhtml')->__('LOCAL'))
+            array('value'=>'http://127.0.0.1:3000/api/v4/', 'label' => Mage::helper('adminhtml')->__('LOCAL')),
+            array('value'=>'https://rc.shiphawk.com/api/v4/', 'label' => Mage::helper('adminhtml')->__('RC'))
         );
     }
 }
