289dd49ef1351ccdc1c383014106bb244863e7f9
George Zhukov <george.zhukov@shiphawk.com>
2019-07-17 15:58:17 +0300

    [#2285] Add custom gateway field to configuration

--- app/code/community/Shiphawk/Order/Model/Source/Gateway.php
+++ app/code/community/Shiphawk/Order/Model/Source/Gateway.php
@@ -11,7 +11,8 @@ class Shiphawk_Order_Model_Source_Gateway
             array('value'=>'https://sandbox.shiphawk.com/api/v4/', 'label' => Mage::helper('adminhtml')->__('Sandbox')),
             array('value'=>'https://stage.shiphawk.com/api/v4/', 'label' => Mage::helper('adminhtml')->__('STAGE')),
             array('value'=>'https://qa.shiphawk.com/api/v4/', 'label' => Mage::helper('adminhtml')->__('QA')),
-            array('value'=>"http://$local_host:3000/api/v4/", 'label' => Mage::helper('adminhtml')->__('LOCAL'))
+            array('value'=>"http://$local_host:3000/api/v4/", 'label' => Mage::helper('adminhtml')->__('LOCAL')),
+            array('value'=>'custom_gateway', label => Mage::helper('adminhtml')->__('Custom Gateway'))
         );
     }
 }
