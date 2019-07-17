0b2aa0a4422c879f22525da0121a733967835856
George Zhukov <george.zhukov@shiphawk.com>
2019-07-17 17:41:56 +0300

    [#2285] Remove local gateway

--- app/code/community/Shiphawk/Order/Model/Source/Gateway.php
+++ app/code/community/Shiphawk/Order/Model/Source/Gateway.php
@@ -4,14 +4,11 @@ class Shiphawk_Order_Model_Source_Gateway
 {
     public function toOptionArray()
     {
-        $local_host = getenv('LOCALHOST_SHIPHAWK') ? getenv('LOCALHOST_SHIPHAWK') : '127.0.0.1';
-
         return array(
             array('value'=>'https://shiphawk.com/api/v4/', 'label' => Mage::helper('adminhtml')->__('Live')),
             array('value'=>'https://sandbox.shiphawk.com/api/v4/', 'label' => Mage::helper('adminhtml')->__('Sandbox')),
             array('value'=>'https://stage.shiphawk.com/api/v4/', 'label' => Mage::helper('adminhtml')->__('STAGE')),
             array('value'=>'https://qa.shiphawk.com/api/v4/', 'label' => Mage::helper('adminhtml')->__('QA')),
-            array('value'=>"http://$local_host:3000/api/v4/", 'label' => Mage::helper('adminhtml')->__('LOCAL')),
             array('value'=>'custom_gateway', label => Mage::helper('adminhtml')->__('Custom Gateway'))
         );
     }
